dbe7511d736f4cca3984f8b26904f97c14285d21
egill15 <egillb86@gmail.com>
2016-12-13 17:48:38 +0000

    Menu Selector

--- game.py
+++ game.py
@@ -15,27 +15,38 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((display_width, display_height))
 
 def introMenu():
-    run = True
-    while run:
-        for event in pygame.event.get():
-            print(event)
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-                
-        screen.fill(white)
-        theFont = pygame.font.Font('freesansbold.ttf',30)
-        newGame = theFont.render("Play Game", True, black)
-        newGameR = newGame.get_rect()
-        newGameR.center = ((display_width/2),(display_height/2))
-        screen.blit(newGame, newGameR)
-
-        if pygame.event.get(pygame.MOUSEBUTTONDOWN):
-                if newGameR.collidepoint(pygame.mouse.get_pos()):
-                        theGame()
+        text = ['Play Game', 'Quit']
+        run = True
+        while run:
+                screen.fill(white)
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        quit()
+                print(event)
+                font = pygame.font.Font('freesansbold.ttf',30)
+                x = display_width/2
+                y = display_height/2
+                isClick = []
+                for t in text:
+                        tex = font.render(t, True, black)
+                        texR = tex.get_rect()
+                        texR.center = (x,y)
+                        if texR.collidepoint(pygame.mouse.get_pos()):
+                                tex = font.render(t, True, red)
+                        screen.blit(tex, texR)
+                        y += 35
+                        isClick.append(texR)
                         
-        pygame.display.update()
-        clock.tick(15)
+                if pygame.event.get(pygame.MOUSEBUTTONUP):
+                        if isClick[0].collidepoint(pygame.mouse.get_pos()):
+                                theGame()
+                        if isClick[1].collidepoint(pygame.mouse.get_pos()):
+                                pygame.quit()
+                                quit()
+                                
+                pygame.display.update()
+                clock.tick(15)
         
 def theGame():
         run = True
