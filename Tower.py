2d46d6279ca7fa90a3b17c3796cf70980137ec59
arnarjojo <arnarjojo@gmail.com>
2016-12-13 14:51:21 +0000

    3 crazy towers

--- Tower.py
+++ Tower.py
@@ -1,2 +1,27 @@
 class Tower:
+
+    def shooter():
+        name = 'Shooter'
+        attack = 10
+        speed = 1.0
+        rang = 10
+        cost = 100
+        pene = 1
+
+    def bomber():
+        name = 'Bomber'
+        attack = 25
+        speed = 2.0
+        rang = 15
+        cost = 150
+        pene = 2
+
+    def sniper():
+        name = 'Sniper'
+        attack = 20
+        speed = 3.0
+        rang = 50
+        cost = 400
+        pene = 4
+
     
